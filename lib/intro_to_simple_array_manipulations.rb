c79bd14efc67b62b9b6dd49ccd7af1922bf444e5
Anthony Mendoza <anthonymendoza9922@gmail.com>
2020-03-30 20:54:05 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_concat(array,array2)
+  array.concat(array2)
+end
+
+def using_insert(hello,array)
+ hello.insert(4,array) 
+  
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,integer)
+  array.delete_at(integer)
+end
